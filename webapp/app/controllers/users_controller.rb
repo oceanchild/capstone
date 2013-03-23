2794de1601ca9fdc498363232f079f4e11720441
charleslu <chao.cn@hotmail.com>
2013-03-23 12:57:46 -0400

    update 'edit profile'

--- webapp/app/controllers/users_controller.rb
+++ webapp/app/controllers/users_controller.rb
@@ -60,6 +60,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(session[:user_id])
+    @user.update_attributes(params[:user])
+    redirect_to root_path
   end
 
   def destroy
