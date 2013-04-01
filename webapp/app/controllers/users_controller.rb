63a83275f703637f2c84339fbb55853e5882fb63
Jing Guo <jingguo@Jings-MacBook-Pro.local>
2013-03-31 21:40:55 -0400

    prescription sql modification

--- webapp/app/controllers/users_controller.rb
+++ webapp/app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
     @numPatient = @patient.count
 
     @note = {}
-    @pillInstance = {}
     _myPatient.each do |_myPatient|
       @note[_myPatient.patient_id] = Note.find_by_sql(["select n.content, n.date_created, u.last_name, u.first_name from notes as n, users as u where n.patient_id = ? and u.id = n.author_id order by n.date_created desc", _myPatient.patient_id])
     end
@@ -30,8 +29,11 @@ class UsersController < ApplicationController
       @primaryContact[_myPatient.patient_id] = User.find_by_sql(["select c.first_name, c.last_name, c.phone, c.address from users as c, associations as j where c.id=j.user_id and j.patient_id=? and c.usertype='Guardian'", _myPatient.patient_id])
     end
 
-    @prescription = Prescription.find_by_sql(["select u.first_name, u.last_name, pa.first_name, pa.last_name, p.direction, p.servings_per_day, p.quantities_per_serving, p.start_date, p.end_date, pills.name from associations as a, users as u, patients as pa, prescriptions as p, pills where u.id=? and a.user_id=u.id and a.patient_id=pa.id and p.patient_id=pa.id and pills.id=p.pill_id",_current_id.to_i])
-  
+    @prescription={}
+    _myPatient.each do |_myPatient|
+      @prescription[_myPatient.patient_id] = Prescription.find_by_sql(["select u.first_name, u.last_name, pa.first_name, pa.last_name, p.direction, p.servings_per_day, p.quantities_per_serving, p.start_date, p.end_date, pills.name from users as u, patients as pa, prescriptions as p, pills where p.doctor_id=u.id and p.patient_id=pa.id and pills.id=p.pill_id and pa.id=?",_current_id.to_i])
+    end
+
     @allPills = Pill.find_by_sql(["select id, name from pills"])
 
     # @heartRate = HeartRate.find_by_sql(["select h.pulse, time(m.last_updated) as last_updated from heart_rates as h, monitored_status as m, associations as a where h.monitored_status_id = m.id and m.patient_id = a.patient_id and a.user_id", _current_id.to_i]).collect{|x| [x.pulse, x.last_updated]}
